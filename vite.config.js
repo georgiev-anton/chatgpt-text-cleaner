ec83b3ed45ba58a246f8edc587ed5b2c5edc5e7c
antongeorgiev <byghoster@gmail.com>
2025-06-21 04:24:16 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
   base: '/chatgpt-text-cleaner/',
-})
+});
